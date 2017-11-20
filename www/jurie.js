f021ad5874a219ba8945727cb8f38e09a138d79e
Marjorie09 <30968989+Marjorie09@users.noreply.github.com>
2017-11-20 09:33:45 +0800

    Add files via upload

---
+++ www/jurie.js
@@ -0,0 +1,7 @@
+
+	function save(){
+		window.localStorage['foods'] = document.getElementById('foods').value;
+	}
+	function remember(){
+		document.getElementById('foods').value = window.localStorage['foods'];
+	}
